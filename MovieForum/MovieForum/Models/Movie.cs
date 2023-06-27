4fcb6aff8928454cde0c5b523003a372680f5830
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-06-27 15:31:54 +0200

    can add release year for new movie

--- MovieForum/MovieForum/Models/Movie.cs
+++ MovieForum/MovieForum/Models/Movie.cs
@@ -8,13 +8,13 @@ public class Movie
     public Guid Id { get; }
     public string Title { get; }
     
-    public int Releaseyear { get; }
+    public int ReleaseYear { get; }
     
 
-    public Movie(string title, int releaseyear)
+    public Movie(string title, int releaseYear)
     {
         Id = Guid.NewGuid();
         Title = title;
-        Releaseyear = releaseyear;
+        ReleaseYear = releaseYear;
     }
 }
