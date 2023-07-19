32f8b347cd810610b545b3b3fa32ed1a544ed97e
StrangerDeer <karmanb96@gmail.com>
2023-07-19 14:45:30 +0200

    resolve null expressions

--- MovieForum/MovieForum/Repositories/MovieRepository.cs
+++ MovieForum/MovieForum/Repositories/MovieRepository.cs
@@ -4,7 +4,7 @@ namespace MovieForum.Repositories;
 
 public class MovieRepository : IMovieRepository<Movie>
 {
-    private HashSet<Movie> _movies = SeedMovies();
+    private HashSet<Movie?> _movies = SeedMovies()!;
 
     private static HashSet<Movie> SeedMovies()
     {
@@ -15,38 +15,38 @@ public class MovieRepository : IMovieRepository<Movie>
         };
     }
 
-    public HashSet<Movie> GetMovies()
+    public HashSet<Movie?> GetMovies()
     {
         return _movies;
     }
 
-    public void AddNewMovie(Movie movie)
+    public void AddNewMovie(Movie? movie)
     {
         _movies.Add(movie);
     }
 
     public Movie? GetMovieById(string id)
     {
-        return _movies.FirstOrDefault(movie => movie.Id.Equals(Guid.Parse(id)));
+        return _movies.FirstOrDefault(movie => movie != null && movie.Id.Equals(Guid.Parse(id)));
     }
 
     public void DeleteMovieById(string id)
     {
-        var movieToDelete = _movies.FirstOrDefault(movie => movie.Id.Equals(Guid.Parse(id)));
+        var movieToDelete = _movies.FirstOrDefault(movie => movie != null && movie.Id.Equals(Guid.Parse(id)));
         if (movieToDelete == null) return;
         _movies.Remove(movieToDelete);
     }
 
-    public void UpdateMovie(string id, Movie updatedMovie)
+    public void UpdateMovie(string id, Movie? updatedMovie)
     {
-        var movieToUpdate = _movies.FirstOrDefault(movie => movie.Id.Equals(Guid.Parse(id)));
+        var movieToUpdate = _movies.FirstOrDefault(movie => movie != null && movie.Id.Equals(Guid.Parse(id)));
         if (movieToUpdate == null) return;
         PropertyInfo[] movieProperties = movieToUpdate.GetType().GetProperties();
         
         UpdateMovieProperties(movieProperties, movieToUpdate, updatedMovie);
     }
 
-    private void UpdateMovieProperties(PropertyInfo[] movieProperties, Movie movieToUpdate, Movie updatedMovie)
+    private void UpdateMovieProperties(PropertyInfo[] movieProperties, Movie movieToUpdate, Movie? updatedMovie)
     {
         foreach (PropertyInfo movieProperty in movieProperties)
         {
--- MovieForum/MovieForum/Services/IMovieService.cs
+++ MovieForum/MovieForum/Services/IMovieService.cs
@@ -2,9 +2,9 @@
 
 public interface IMovieService
 {
-    HashSet<Movie> GetMovies();
-    void AddNewMovie(Movie movie);
-    Movie GetMovieById(string id);
+    HashSet<Movie?> GetMovies();
+    void AddNewMovie(Movie? movie);
+    Movie? GetMovieById(string id);
     void DeleteMovieById(string id);
-    void UpdateMovie(string id, Movie updatedMovie);
+    void UpdateMovie(string id, Movie? updatedMovie);
 }
