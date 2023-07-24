481aa7346ea46b7b45839287c5e77734a7618aca
StrangerDeer <113778183+StrangerDeer@users.noreply.github.com>
2023-07-24 14:59:18 +0200

    create database methode; add; get all; get by id; remove; cant modify details

--- MovieForum/MovieForum/Program.cs
+++ MovieForum/MovieForum/Program.cs
@@ -7,12 +7,18 @@ using MovieForum.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Connect db
+builder.Services.AddDbContext<MovieContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IMovieRepository<Movie>, MovieRepository>();
 builder.Services.AddSingleton<IGenreRepository<Genre>, GenreRepository>();
-builder.Services.AddTransient<IMovieService, MovieService>();
+/*builder.Services.AddTransient<IMovieService, MovieService>();*/
+builder.Services.AddTransient<IMovieService, MovieDbService>();
 builder.Services.Configure<FormOptions>(o => {  
     o.ValueLengthLimit = int.MaxValue;  
     o.MultipartBodyLengthLimit = long.MaxValue;  
@@ -21,10 +27,7 @@ builder.Services.Configure<FormOptions>(o => {
 
 builder.Services.AddTransient<IGenreService, GenreService>();
 
-//Connect db
-builder.Services.AddDbContext<MovieContext>(options => 
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
-    );
+
 
 var app = builder.Build();
 
