8ed52670b069acaadae07e5d10b4f47b81495e8c
rBabett <richterbabett03@gmail.com>
2023-07-25 13:48:19 +0200

    fix problems with date of creation

--- MovieForum/MovieForum/Models/Entities/Movie.cs
+++ MovieForum/MovieForum/Models/Entities/Movie.cs
@@ -14,7 +14,7 @@ public class Movie
     public int ReleaseYear { get; set; }
     public string Story { get; set; }
     public double Ratings { get; set; }
-    public DateTime DateOfCreation { get; set; }
+    public DateTime DateOfCreation { get; set; } = DateTime.UtcNow;
     public string? MovieImage { get; set; }
     public string? Genre { get; set; }
     public Movie(string title, int releaseYear, string story, double ratings)
@@ -24,7 +24,6 @@ public class Movie
         ReleaseYear = releaseYear;
         Story = story;
         Ratings = ratings;
-        DateOfCreation = DateTime.Now;
         Genre = null;
         MovieImage = null;
     }
