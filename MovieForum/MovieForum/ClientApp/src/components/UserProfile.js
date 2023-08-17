12d5a3a3edc9e551969b1cf6a275e95f4062fb37
rBabett <richterbabett03@gmail.com>
2023-08-17 18:07:40 +0200

    add user profile picture to the project

--- MovieForum/MovieForum/ClientApp/src/components/UserProfile.js
+++ MovieForum/MovieForum/ClientApp/src/components/UserProfile.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate, useParams} from "react-router-dom";
 import EditMovieDetails from "./EditMovieDetails";
+import profilePicture from '../User/noimage.jpg';
 const UserProfile = () => {
     const navigate = useNavigate();
     const userObj = {
@@ -38,7 +39,7 @@ const UserProfile = () => {
     return <div className="user-container">
             <div className="user-card">
                 <img
-                    src="https://lh3.googleusercontent.com/oUUiPB9sq3ACq4bUaRmo8pgvC4FUpRRrQKcGIBSOsafawZfRpF1vruFeYt6uCfL6wGDQyvOi6Ez9Bpf1Fb7APKjIyVsft7FLGR6QqdRFTiceNQBm1In9aZyrXp33cZi9pUNqjHASdA=s170-no"
+                    src={profilePicture}
                     alt="Person" className="card__image"/>
                     <p className="card__name">User Name</p>
                     <p className="email">Email@Address.com</p>
